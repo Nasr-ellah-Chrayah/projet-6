3e14483188b1667234e7ff002c90b44a8adde0de
Nasr-ellah Chrayah <92031858+Nasr-ellah-Chrayah@users.noreply.github.com>
2021-11-29 23:16:55 +0100

    Update

---
+++ Realisation/app.js
@@ -0,0 +1,12 @@
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult')
+var lowOrHigh = document.querySelector('.lowOrHigh')
+var guessSubmit = document.querySelector('.guessSubmit')
+var guessField = document.querySelector('.guessField')
+var guessCount = 1;
+var resetButton;
+
+guessField.focus()
+
+function checkguesses 
